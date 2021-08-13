ee41ca0c9621dc655a414c3b6a18f125b798c996
Jon <jon.bianco@trustcommerce.com>
2021-08-13 14:36:38 -0700

    Initial Repository

---
+++ README.md
@@ -0,0 +1,33 @@
+# README #
+
+This an application for converting a byte [] to an image.
+
+### What is this repository for? ###
+
+* IMAGE CONVERTER APPLICATION
+* 1.00.0.(100)
+* git remote add origin https://github.com/web-projects/IMAGE_CONVERTER.git
+
+### How do I get set up? ###
+
+* Summary of set up
+* Configuration
+* Dependencies
+* Database configuration
+* How to run tests
+* Deployment instructions
+
+### Contribution guidelines ###
+
+* Writing tests
+* Code review
+* Other guidelines
+
+### GIT NOTES ###
+
+*  AUTO-CONVERTING CRLF line endings into LF
+   $ git config --global core.autocrlf true
+   
+### HISTORY ###
+
+* 20210813 - Initial repository
---
+++ Source/ImageConverter/ImageConverter/Engine/ImageConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace ImageConverter.Engine
+{
+    public class ImageConverterEngine
+    {
+        readonly string SignatureFilename = "signature.png";
+        readonly string filePath;
+        readonly string fileName;
+
+        public ImageConverterEngine()
+        {
+            filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "output");
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+            fileName = Path.Combine(filePath, SignatureFilename);
+        }
+
+        public bool ConvertByteArrayToImage(byte[] byteArrayIn)
+        {
+            using (MemoryStream ms = new MemoryStream(byteArrayIn))
+            {
+                using (var fs = new FileStream(fileName, FileMode.Create))
+                {
+                    ms.WriteTo(fs);
+                }
+            }
+
+            return File.Exists(fileName);
+        }
+
+        public void ImageDisplay()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+
+            ProcessStartInfo psi = new ProcessStartInfo("rundll32.exe",
+                String.Format("\"{0}{1}\", ImageView_Fullscreen {2}",
+                    Environment.Is64BitOperatingSystem ? path.Replace(" (x86)", "") : path,
+                    @"\Windows Photo Viewer\PhotoViewer.dll",
+                    fileName));
+
+            psi.UseShellExecute = false;
+
+            Process viewer = Process.Start(psi);
+
+            // cleanup when done...
+            viewer.EnableRaisingEvents = true;
+            viewer.Exited += (o, args) =>
+            {
+                File.Delete(fileName);
+            };
+        }
+    }
+}
